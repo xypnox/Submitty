64f43d828524c7463ba3a893a2965694e1480948
Victor Nazzaro <victor741naruto@gmail.com>
2018-08-25 14:35:48 -0400

    Made new function

--- site/app/controllers/student/PDFController.php
+++ site/app/controllers/student/PDFController.php
@@ -16,7 +16,40 @@ class PDFController extends AbstractController {
             case 'download_annotated_pdf':
                 $this->downloadAnnotatedPdf();
                 return;
+            case 'get_annotations':
+                $this->getAnnotations();
+                break;
         }
+    }
+
+    /**
+     * @param {string} user_id The student's userid
+     * @param {string} gradeable_id The gradeable's id
+     * @param {string, optional} instructor_id This field is used if only one annotation is required
+     *
+     * @return array All the annotations specified in JSON
+     */
+    private function getAnnotations(){
+        $user_id = $_POST['user_id'] ?? NULL;
+        $gradeable_id = $_GET['gradeable_id'] ?? NULL;
+        $active_version = $this->core->getQueries()->getGradeable($gradeable_id, $user_id)->getActiveVersion();
+        $annotation_path = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'annotations', $gradeable_id, $user_id, $active_version);
+        $annotation_jsons = [];
+        if(is_dir($annotation_path)){
+            $first_file = scandir($annotation_path)[2];
+            $annotation_path = FileUtils::joinPaths($annotation_path, $first_file);
+            if(is_file($annotation_path)) {
+                $dir_iter = new \DirectoryIterator(dirname($annotation_path . '/'));
+                foreach ($dir_iter as $fileinfo) {
+                    if (!$fileinfo->isDot()) {
+                        $grader_id = preg_replace('/\\.[^.\\s]{3,4}$/', '', $fileinfo->getFilename());
+                        $grader_id = explode('_', $grader_id)[1];
+                        $annotation_jsons[$grader_id] = file_get_contents($fileinfo->getPathname());
+                    }
+                }
+            }
+        }
+        return $annotation_jsons;
     }
 
     private function downloadAnnotatedPdf(){
@@ -24,13 +57,13 @@ class PDFController extends AbstractController {
         return "test";
     }
 
+    //Deprecated
     private function showAnnotatedPdf(){
         $gradeable_id = $_GET['gradeable_id'] ?? NULL;
         $user_id = $this->core->getUser()->getId();
         $filename = $_GET['file_name'] ?? NULL;
         $active_version = $this->core->getQueries()->getGradeable($gradeable_id, $user_id)->getActiveVersion();
         $annotation_path = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'annotations', $gradeable_id, $user_id, $active_version);
-        $first_file = scandir($annotation_path)[2];
         $annotation_jsons = [];
         if(is_dir($annotation_path)){
             $first_file = scandir($annotation_path)[2];
