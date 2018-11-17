21a9f0bd95bba2c83488b6678da0e95fad8b51d9
Victor Nazzaro <victor741naruto@gmail.com>
2018-11-16 21:31:08 -0500

    Fixed can't vertical scroll problem

--- site/public/js/pdf/PDFAnnotateEmbedded.js
+++ site/public/js/pdf/PDFAnnotateEmbedded.js
@@ -72,6 +72,7 @@ function render(gradeable_id, user_id, grader_id, file_name, page_num, url = "")
                 $("a[value='zoomcustom']").text(parseInt(window.RENDER_OPTIONS.scale * 100) + "%");
                 viewer.innerHTML = '';
                 NUM_PAGES = pdf.pdfInfo.numPages;
+
                 for (let i=0; i<NUM_PAGES; i++) {
                     let page = UI.createPage(i+1);
                     viewer.appendChild(page);
@@ -79,7 +80,9 @@ function render(gradeable_id, user_id, grader_id, file_name, page_num, url = "")
                     UI.renderPage(page_id, window.RENDER_OPTIONS).then(([pdfPage, annotations]) => {
                         let viewport = pdfPage.getViewport(window.RENDER_OPTIONS.scale, window.RENDER_OPTIONS.rotate);
                         PAGE_HEIGHT = viewport.height;
-                        $('#file_content').animate({scrollTop: page_num*PAGE_HEIGHT}, 500);
+                        if(i == page_num) {
+                            $('#file_content').animate({scrollTop: page_num * PAGE_HEIGHT}, 500);
+                        }
                     }).then(function(){
                         document.getElementById('pageContainer'+page_id).addEventListener('mousedown', function(){
                             //Makes sure the panel don't move when writing on it.
